30b50effe40d130e72aebd906726fa2955dc6b5c
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2019-09-08 10:28:14 +0530

    implement logging

---
+++ scripts/login.php
@@ -0,0 +1,11 @@
+<?php
+klog("It's like a triple staged darkness");
+
+
+
+function klog($message){
+    //todo: Add date and time
+    //todo: create a new file for each date
+    file_put_contents('logs/log.txt', $message.PHP_EOL , FILE_APPEND);
+}
+?>
