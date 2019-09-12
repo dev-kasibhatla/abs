d94a45b9c3a5b7273fdb6845eb32d3408905edde
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2019-09-12 23:24:23 +0530

    clean up

--- account.php
+++ account.php
@@ -11,16 +11,10 @@
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/account.css" rel="stylesheet" type="text/css">
 
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body onLoad="initialize()">
-<p id = "onload_message_para">
-	<h1>Please wait</h1>
+<p >
+	<h1 id = "onload_message_para">Please wait</h1>
 </p>
 <div class="header">
  
@@ -77,7 +71,7 @@
  	</div>
  	<div class="row" style="margin-top: 2%">
  		<div class="col-md-offset-3 col-md-9">
-			<a href="#">Change Password</a>
+			<a href="acc_change_password.php">Change Password</a>
 		</div>	
  	</div>
  </div>	
