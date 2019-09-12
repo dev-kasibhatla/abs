59963a468b2adc9630919693815f87f6754aee8d
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2019-09-12 15:11:53 +0530

    finish login routine

--- js/login.js
+++ js/login.js
@@ -1,48 +1,81 @@
 var scale = 'scale(0.90)';
-document.querySelector("#mainbody").style.webkitTransform =  scale;    // Chrome, Opera, Safari
- document.querySelector("#mainbody").style.msTransform =   scale;       // IE 9
- document.querySelector("#mainbody").style.transform = scale;     // General
 
 //start
-
-function signupSubmitClicked(){
-	document.getElementById("login_message").innerHTML="Button clicked";
-	//execute php script
-	//$.post("js/php/validate.php");
-	//$.get("js/php/validate.php");
-	//$.load("js/php/validate.php");
-	//$("p").hide();
-	//using jquery with ajax
-	$.POST("js/php/validate.php");
-	
-}
-
-
 function initialize(){
 	$(document).ready(function(){
-		document.getElementById("login_message").innerHTML="Page has loaded";
-		
-	});
+        $("#login_message").hide();
+    });
 }
 
-//script to validate password
-var passInput = document.getElementsByName("signup_group_password");
-passInput.onfocus = function() {
-	document.getElementById("signup_password_error").style.display=none;
-	document.getElementById("login_message").innerHTML="Button clicked";
 
-};
-passInput.onblur = function() {
-	document.getElementById("signup_password_error").style.display = none;
-};
 
-function passwordKeyUp(){
-	document.getElementById("signup_password_error").style.display=none;
-}
+//jquery code to submit login form:
+var request;
 
-function passwordOnFocus(){
-	document.getElementById("signup_password_error").style.display=none;
+$("#login_form").submit(function(event){
+    $("#login_message").hide();
+	console.log("Submitting login data now");
+    // Prevent default posting of form - put here to work in case of errors
+    event.preventDefault();
 
-}
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $form = $(this);
+
+    // Let's select and cache all the fields
+    var $inputs = $form.find("input, select, button, textarea");
+	
+    // Serialize the data in the form
+    var serializedData = $form.serialize();
+
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Note: we disable elements AFTER the form data has been serialized.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "../scripts/login-validate.php",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log(response);
+        if(response == 1){
+            //user is logged in
+			//redirect to account page
+			window.location.replace("account.php");
+
+        }else{
+            //user not logged in. Redirect to login
+			console.log("Invalid login");
+			$("#login_message").show();        }
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+        
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+
+});
+
 
 
