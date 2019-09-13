b58ac5f98fca5b2c6adab2fa5264902f807509aa
Aditya Kasibhatla <dev.kasibhatla@gmail.com>
2019-09-13 19:40:13 +0530

    Admin panel

---
+++ adpa2.php
@@ -0,0 +1,71 @@
+<?php
+	session_start();
+	if(isset($_SESSION["adminID"])){
+		echo "User ID: ".$_SESSION["adminID"];
+	}else{
+		//not logged in!
+		echo '<script type="text/JavaScript">  
+		 window.location.replace("adpa.php");
+		 </script>' 
+		;
+		die();
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Auditorium: Admin</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/account.css">
+
+</head>
+<body>
+<div class="header"> 
+  <a href="#default" class="logo" id="header_logo">Account</a>
+  <div class="header-right">
+    <a href="index.html" style="font-weight: bold">Home</a>
+	<a href= "index.html" onclick="logout()" style="font-weight: bold" id="top_login_link">Logout</a>
+  </div>
+</div>
+
+
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-3 col-md-offset-2">
+      <h1 class="text-capitalize" id = "account_name_label">Admin Panel</h1>
+    </div>
+  </div>
+  <hr>
+</div>
+
+<div class="container-fluid col-md-offset-2 col-md-4" >
+ 	<div class="row">
+ 		<div class="col-md-9 col-md-offset-0"  style="margin-bottom:4%">
+			<h2>Actions</h2>
+		</div>		
+ 	</div>
+
+ 	<div class="row" style="margin-top: 4%">
+ 		<div class="col-md-9 col-md-offset-0">
+			<a href="#">Generate slots in DB</a>
+		</div>	
+ 	</div>
+ 
+ 
+ </div>	
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="js/jquery-1.11.3.min.js"></script> 
+<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/bootstrap.js"></script>
+<script src="js/account.js"></script>
+
+</body>
+</html>
