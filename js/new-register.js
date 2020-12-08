37d2b4f20080ed198cf159f756fe845f36f260b2
Prometheus0102 <46524809+Prometheus0102@users.noreply.github.com>
2020-12-09 00:55:10 +0530

    Create new-register.js
    new js file for register.html with undecided php file paths

---
+++ js/new-register.js
@@ -0,0 +1,211 @@
+function initialize() {
+    getSchools();
+}
+
+
+
+
+
+var request;
+var a  =  $("input");
+$(a).focus(function() {
+   $(this).css('border',"none"); 
+});
+
+function getSchools(){
+   
+    request = $.ajax({
+        url: "../scripts/register-data.php",
+        type: "post",
+        data: {event:"schoolData"}
+    });
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log(response);
+        if(response.length > 0){
+            var schools = JSON.parse(response);
+            var select = $("#inputSchool");
+            for(var i=0;i<schools.length;i++)
+            {
+                $(select).append("<option value=\"i+1\">"+schools[i]+"</option>")
+            }
+
+        }else{
+            //user not logged in. Redirect to login
+            console.log("Could not fetch schools data");
+        }
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+        
+    });
+
+}
+
+
+
+$("#btnSubmit").click(function(){
+
+    // $("#login_message").hide();
+    $("#emailHelp").html("");
+    $("#errorMentor").html("");
+    $("#errorGroupDetails").html("");
+    $("#errorPassword").html("");
+    
+
+    
+	console.log("Submitting register data now");
+    // Prevent default posting of form - put here to work in case of errors
+    event.preventDefault();
+
+    // Abort any pending request
+    if (request) {
+        request.abort();
+    }
+    // setup some local variables
+    var $f = $('.registerForms');
+    var $inputs;
+
+    // Let's select and cache all the fields
+    Array.prototype.forEach.call($f,form => {
+        
+        $inputs = $(form).find("input, select, button, textarea");
+        const re = /^(([^<\*>\'(=)\[\]\\.,;:\s"0-9@]+(\.[^<>()\[\]\\.,;:\s"]+)*)|(".+"))$/;
+        const repass = /^(([^<>\'(=)\[\]\\.,;:\s"]+(\.[^<>()\[\]\\.,;:\s"]+)*)|(".+"))$/;
+        for(var i=0;i<$inputs.length;i++)
+        {
+            switch ($inputs[i].type) {
+                case "email":
+                {
+                    if($($inputs[i]).parent().parent().attr('id')=="form1")
+                    {
+                        if(($inputs[i].value.length < 5))
+                        {
+                            $("#emailHelp").html("<strong class=\"text-danger\">Email should have more characters </strong>");
+                            $($inputs[i]).css('border',"2px solid red");
+                        }
+                    }
+                    else
+                    {
+                        if(($inputs[i].value.length < 5))
+                        {
+                            $("#errorMentor").html("<strong class=\"text-danger\">Email should have more characters </strong>");
+                            $($inputs[i]).css('border',"2px solid red");
+                        }
+                    }
+                        
+                    break;
+                }
+                case "password":
+                {
+                    if(!repass.test($inputs[i].value))
+                    {
+                        $("#errorPassword").html("<strong class=\"text-danger\">Please enter a valid password with the appropriate characters</strong>");
+                        $($inputs[i]).css('border',"2px solid red");  
+                    }
+                    else if(($inputs[i].value.length < 8))
+                    {
+                        $("#errorPassword").html("<strong class=\"text-danger\"> Password has to have more than eight characters </strong>");
+                        $($inputs[i]).css('border',"2px solid red");
+                    }
+                    break;     
+                }        
+                case "text":   
+                {
+                	if($($inputs[i]).parent().parent().attr('id')=="form2")    
+                    {
+
+                        if(!repass.test($inputs[i].value))
+                        {
+                            $("#errorGroupDetails").html("<strong class=\"text-danger\">Please enter valid group details</strong>")  ;
+                            $($inputs[i]).css('border',"2px solid red");
+                        }
+                        else if(($inputs[i].value.length < 7))
+                        {
+                            var a = $("label[for='"+$($inputs[0]).attr('id')+"']").text()
+                            $("#errorGroupDetails").html("<strong class=\"text-danger\">"+ a +" should have atleast 7 characters  </strong>");
+                            $($inputs[i]).css('border',"2px solid red");
+                        }
+                    }
+                    else
+                    {
+                        if(!repass.test($inputs[i].value))
+                        {
+                            $("#errorMentor").html("<strong class=\"text-danger\">Please enter valid mentor details</strong>")  ;
+                            $($inputs[i]).css('border',"2px solid red");
+                        }
+                        else if(($inputs[i].value.length < 7))
+                        {
+                            var a = $("label[for='"+$($inputs[0]).attr('id')+"']").text()
+                            $("#errorMentor").html("<strong class=\"text-danger\">"+ a +" should have atleast 7 characters  </strong>");
+                            $($inputs[i]).css('border',"2px solid red");
+                        }    
+                    }        
+                    break;
+                }
+                case "tel":   
+                {
+                    if($inputs[i].value.length<10)
+                    {
+                        $("#phoneHelp").html("<strong class=\"text-danger\">Please enter a valid 10 digit phone number</strong>");
+                        $($inputs[i]).css('border',"2px solid red");
+                    }
+                    break;
+                }
+            }
+        }
+    });
+    
+	
+    // Serialize the data in the form
+    var serializedData = $('#form1,#form2,#form3,#form4').serializeArray();
+    console.log(serializedData);
+    // Let's disable the inputs for the duration of the Ajax request.
+    // Disabled form elements will not be serialized.
+    $inputs.prop("disabled", true);
+
+    // Fire off the request to /form.php
+    request = $.ajax({
+        url: "../scripts/login-validate.php",
+        type: "post",
+        data: serializedData
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        console.log(response);
+        if(response == 1){
+            //user is logged in
+			//redirect to account page
+			window.location.replace("account.php");
+
+        }else{
+            //user not logged in. Redirect to login
+			console.log("Invalid login");
+			$("#login_message").show();        }
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+        console.error(
+            "The following error occurred: "+
+            textStatus, errorThrown
+        );
+        
+    });
+
+    // Callback handler that will be called regardless
+    // if the request failed or succeeded
+    request.always(function () {
+        // Reenable the inputs
+        $inputs.prop("disabled", false);
+    });
+});
