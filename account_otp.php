4db90141463310b306a09defef30cda555b66b6e
Manas Sambhus <51392775+msvamp@users.noreply.github.com>
2020-12-11 01:15:53 +0530

    Separate db-connects, add htaccess, add includes folder
    - Removed old DB credentials from all files
    - Auto-Removed unnecessary whitespaces from processed files (Todo: SPACE->TAB)
    + Added htaccess at root for global configuration
    + Added "includes" folder and "includes.php" for shared code
    + Added gitignore to protect new DB credentials

--- account_otp.php
+++ account_otp.php
@@ -3,11 +3,11 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$mEmail = ($_POST["mentor_email"]);
 		$mOtp = ($_POST["mentor_otp"]);
-		
+
 		//retrieve data from server
 		$table_name = "grps";
         $sql = "select `OTP`,`Activated` from $table_name where `Mentor Email` = '$mEmail' ";
-		$i = mysqli_connect('localhost','id10814660_root','dFX0#HxYkm(Y*g&I','id10814660_abs','3306');
+		$i = my_sqli_connect();
 		if($i -> connect_error){
 			klog("Unable to connect to DB");
 			echo "Error connecting to mysql server";
@@ -21,45 +21,45 @@
 			if($activated == 1){
 				echo "Account already activated. Redirecting.";
 				klog("Account already activated");
-				echo '<script type="text/JavaScript">  
+				echo '<script type="text/JavaScript">
 				 window.location.replace("index.html");
-				 </script>' 
+				 </script>'
 				;
 				die();
 			}
-			
+
 			//verify
 			if($otp == $mOtp){
 				//set account as activated
-				$i=mysqli_connect('localhost','id10814660_root','dFX0#HxYkm(Y*g&I','id10814660_abs','3306');
+				$i=my_sqli_connect();
 
 				klog("Otp matches server data. Activating this account");
 				$sql = "update $table_name set `Activated` = '1' where `Mentor Email` = '$mEmail' ";
 				$result = mysqli_query($i,$sql);
-				
-				$i=mysqli_connect('localhost','id10814660_root','dFX0#HxYkm(Y*g&I','id10814660_abs','3306');
+
+				$i=my_sqli_connect();
 
 				$sql = "update $table_name set `OTP` = '0' where `Mentor Email` = '$mEmail' ";
 				$result = mysqli_query($i,$sql);
-				//start a mentor session and make them set password	
+				//start a mentor session and make them set password
 				session_start();
 				$_SESSION["username"] = $mEmail;
             	$_SESSION["level"] = 1;
-				echo '<script type="text/JavaScript">  
+				echo '<script type="text/JavaScript">
 				 window.location.replace("change_password.php");
-				 </script>' 
+				 </script>'
 				;
 				die();
-				
+
 			}else{
 				echo "Invalid OTP. Try again";
 			}
 		}else{
 			echo "Invalid login";
 			klog("Invalid email");
-			echo '<script type="text/JavaScript">  
+			echo '<script type="text/JavaScript">
 				 window.location.replace("adpa.php");
-				 </script>' 
+				 </script>'
 				;
 			die();
 		}
@@ -89,7 +89,7 @@ function klog($message){
 
 </head>
 <body>
-<div class="header"> 
+<div class="header">
   <a href="#default" class="logo" id="header_logo">Account</a>
   <div class="header-right">
     <a href="index.html" style="font-weight: bold">Home</a>
@@ -100,20 +100,20 @@ function klog($message){
 		<div class="col-md-offset-0 col-md-12">
 		  <h1 class="text-capitalize">Enter these details to confirm your Account</h1>
 		</div>
-		
+
   <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
  		<div class="col-md-offset-5 col-md-4" >
  		  <div>
  		    <div class="input-group col-md-11 col-md-offset-0" style="margin-bottom:4%"><span class="input-group-addon">Mentor Email</span>
  		      <input type="email" class="form-control" aria-describedby="addon1" name="mentor_email" required>
 	        </div>
- 		      
+
  		   <div class="input-group col-md-11 col-md-offset-0" style="margin-bottom:2%"><span class="input-group-addon">OTP</span>
  		     <input type="text" class="form-control" aria-describedby="addon1" name="mentor_otp" required>
 	        </div>
-	        
+
 	      </div>
-	      
+
 		</div>
  		<div class="col-md-offset-5 col-md-4">
 				<button type="submit" class="btn btn-primary btn-block" id ="admin_submit">Submit</button>
@@ -121,11 +121,11 @@ function klog($message){
 			<div class="col-md-offset-5 col-md-4">
 				<p>Upon entering the correct OTP, you'll be asked to set a new password.<br>Leave old password blank</p>
 	        </div>
-					
+
 		</form>
 </div>
-<script src="js/jquery-1.11.3.min.js"></script> 
-<!-- Include all compiled plugins (below), or include individual files as needed --> 
+<script src="js/jquery-1.11.3.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.js"></script>
 
 </body>
