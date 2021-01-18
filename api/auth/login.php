f1d79c80ebd7e15b6b44821a58c41389fd885751
Manas Sambhus <51392775+msvamp@users.noreply.github.com>
2021-01-19 00:33:44 +0530

    Added slot booking script, bug-fixes
    - Change PHP error options
    - Add slot booking script
    - Bug fixes in login, functions and slots
    ! Input validation in book.php is pending

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'auth.php';
-
 denyauth();
 
 $email=$_POST['email']??NULL;
--- includes/functions.php
+++ includes/functions.php
@@ -7,7 +7,7 @@ const page=['<html><head><title>','</title><meta name="viewport" content="width=
 '</div></div></body></html>'];
 
 // Exception handler used everywhere
-set_exception_handler(function($e) use($page) {
+set_exception_handler(function($e) {
 	if(!empty($e)) {
 		$c=$e->getCode();
 		if($c && in_array($c,[100,101,102,200,201,202,203,204,205,206,207,208,226,300,301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,421,422,423,424,426,428,429,431,444,451,499,500,501,502,503,504,505,506,507,508,510,511,599],1))
@@ -25,14 +25,13 @@ set_exception_handler(function($e) use($page) {
 		exit(json_encode(['error'=>$m]));
 	}
 	header('Content-Type: text/html');
-	exit($page[0].'Error'.$page[1].'red'.$page[2].nl2br(htmlspecialchars($m)).$page[3]);
+	exit(page[0].'Error'.page[1].'red'.page[2].nl2br(htmlspecialchars($m)).page[3]);
 });
 
 function sexit($s) {
-	global $page;
 	if(empty($_SERVER['HTTP_ACCEPT']) || (strpos($_SERVER['HTTP_ACCEPT'],"text/html")===false && strpos($_SERVER['HTTP_ACCEPT'],"application/xhtml+xml")===false)) {
 		header('Content-Type: application/json');
 		exit(json_encode(['success'=>$s]));
 	}
-	exit($page[0].'Success'.$page[1].'green'.$page[2].nl2br(htmlspecialchars($s)).$page[3]);
+	exit(page[0].'Success'.page[1].'green'.page[2].nl2br(htmlspecialchars($s)).page[3]);
 }
