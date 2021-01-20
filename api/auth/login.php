203f373ceaf405ffc8d3bf4ea09e1db77fa051bb
Manas Sambhus <51392775+msvamp@users.noreply.github.com>
2021-01-21 00:50:14 +0530

    Multiple changes to login in auth system

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,11 +1,27 @@
 <?php
 require_once 'auth.php';
-initauth();
+$x=initauth(true);
+
+if($x && empty($_GET) && empty($_POST)) {
+	header('Content-Type: application/json');
+	exit(json_encode([
+		'email'=>$_SESSION['email'],
+		'phone'=>$_SESSION['phone'],
+		'name'=>$_SESSION['name'],
+		'rname'=>$_SESSION['rname'],
+		'remail'=>$_SESSION['remail'],
+		'rschool'=>$_SESSION['rschool'],
+		'rdept'=>$_SESSION['rdept'],
+		'ename'=>$_SESSION['ename'],
+		'lastlogin'=>$_SESSION['lastlogin'],
+		'lastloginip'=>$_SESSION['lastloginip']
+	]));
+}
 
 $email=$_POST['email']??NULL;
 if( !is_string($email) ||
 	strlen($email)>A_EMAIL_MAX ||
-	!boolval($email=filter_var($email,FILTER_VALIDATE_EMAIL))
+	!($email=filter_var($email,FILTER_VALIDATE_EMAIL))
 )
     throw new Exception(A_EM_ERR,400);
 
@@ -16,10 +32,27 @@ $q=$db->query("SELECT * FROM `club` WHERE `email`='$em'");
 if( $q->num_rows===0 || !(($q=$q->fetch_assoc())['enabled']) ||
     !password_verify($_POST['password']??null,$q['password'])
 )
-	throw new Exception("This account does not exist",401);
+	throw new Exception("Invalid credentials or account disabled",401);
+
+$db->autocommit(true);
+$db->query("UPDATE `club` SET `lastlogin`=NOW(),`lastloginip`=INET6_ATON('".
+	$db->escape_string($_SERVER['REQUEST_ADDR'])
+."')");
 
 session_start();
 $_SESSION=$q;
 // To-do: Remove useless data, add and validate login timestamp
 session_commit();
-sexit("Welcome ".$q['name']);
+
+exit(json_encode([
+	'email'=>$_SESSION['email'],
+	'phone'=>$_SESSION['phone'],
+	'name'=>$_SESSION['name'],
+	'rname'=>$_SESSION['rname'],
+	'remail'=>$_SESSION['remail'],
+	'rschool'=>$_SESSION['rschool'],
+	'rdept'=>$_SESSION['rdept'],
+	'ename'=>$_SESSION['ename'],
+	'lastlogin'=>$_SESSION['lastlogin'],
+	'lastloginip'=>$_SESSION['lastloginip']
+]));
--- includes/auth.php
+++ includes/auth.php
@@ -6,9 +6,10 @@ define('A_NM_ERR',"Name must be a string with 1-".A_NAME_MAX." characters");
 define('A_EM_ERR',"Email must be valid with 1-".A_EMAIL_MAX." characters");
 
 function initauth($lock=true) {
+	if(session_status()===PHP_SESSION_ACTIVE)
+		return true;
 	if(array_key_exists(session_name(),$_COOKIE))
 		session_start(['read_and_close'=>!$lock]);
-
 	if(($lock && session_status()===PHP_SESSION_ACTIVE) || (!$lock && !empty($_SESSION)))
 		return true;
 	return false;
