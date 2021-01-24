8a95e29da541eecc2482a0fd0774e152014fef4a
Prometheus0102 <46524809+Prometheus0102@users.noreply.github.com>
2021-01-24 12:07:33 +0530

    logged in check

--- js/events.js
+++ js/events.js
@@ -1,4 +1,5 @@
 var clubData;
+var request;
 function checkLogin() {
     if(request)
         request.abort();
@@ -14,13 +15,20 @@ function checkLogin() {
     });
     request.fail(function (jqXHR, textStatus, error){
         // Log the error to the console
-        window.location.replace('login.html');
-
+//         window.location.replace('login.html');
+        $($(".nav-item")[0]).html("<a class=\"nav-link\" href=\"login.html\">Login<span class=\"sr-only\">(current)</span></a>");
+        $($(".nav-item")[1]).html("<a class=\"nav-link\" href=\"register.html\">Register<span class=\"sr-only\">(current)</span></a>");
+        $($(".nav-item")[1]).addClass("active");
+        $($(".nav-item")[1]).removeClass("dropdown");
+        console.log("Not logged in");
     });
 
 }
 function initialize() {
     // note: using this class as a reference can be dangerous in the future
-    $('.flat-glass').matchHeight();
     checkLogin();
+    $('.flat-glass').matchHeight();
+
 }
+
+$(document).ready(initialize);
