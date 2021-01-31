f3fab1d504086a8b6848e2fcc7b879ae649eace0
Prometheus0102 <46524809+Prometheus0102@users.noreply.github.com>
2021-01-31 16:32:27 +0530

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 var clubData;
 var request;
+var clubEvents;
 function checkLogin() {
     if(request)
         request.abort();
@@ -24,9 +25,34 @@ function checkLogin() {
     });
 
 }
+function getEvents() {
+    if(request)
+        request.abort();
+    request = $.ajax({
+        url:"../api/auth/events.php",
+        type:'get'
+    });
+    request.done(function (response, textstatus,jqXHR){
+        console.log(response);
+        clubEvents = response;
+        for(let i=0;i<Object.keys(clubEvents).length;i++){
+           for (keys in clubEvents[i]['tslot']){
+               console.log(new Date(keys));
+            }
+        }
+        
+
+
+    });
+    request.fail(function (jqXHR, textStatus, error){
+        
+    });
+        
+}
 function initialize() {
     // note: using this class as a reference can be dangerous in the future
     checkLogin();
+    getEvents();
     $('.flat-glass').matchHeight();
 
 }
