2d76474a3898e75565b862d66c9bbcfe54d8cb8e
Prometheus0102 <46524809+Prometheus0102@users.noreply.github.com>
2021-02-01 13:42:51 +0530

    @adi check

--- js/index.js
+++ js/index.js
@@ -1,5 +1,70 @@
 var clubData;
 var request;
+var calendar;
+var tinySlots;
+function getSlotKey(z){
+    return z.getFullYear().toString() + "-" +
+        (z.getMonth() + 1).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        }) + "-" +
+        z.getDate().toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        })
+}
+
+
+function tinySchedule()
+{
+
+    if(request)
+        request.abort();
+    let limits = calendar.getLimits();
+    request = $.ajax({
+        url:"../api/tinysched.php",
+        type:"post",
+        data:{edate:getSlotKey(limits[1]),sdate:getSlotKey(limits[0])}
+    });
+    request.done(function (response, textstatus,jqXHR){
+        console.log(response);
+        tinySlots = response;
+        let badgeSetter=[];
+        for(keys in tinySlots)
+        {
+//         	console.log(keys);
+//         	console.log(tinySlots[keys]);
+            if(tinySlots[`${keys}`]==true)
+            {
+//         		console.log("True");
+                badgeSetter.push(1);
+            }
+            else
+            {
+//         		console.log("False");
+                badgeSetter.push(0);
+            }
+            if(badgeSetter.length==30)
+            {
+                console.log(badgeSetter);
+                calendar.setBadges(badgeSetter);
+                calendar.render();
+                break;
+            }
+
+        }
+    });
+    request.fail(function (jqXHR, textStatus, error){
+        // Log the error to the console
+//         window.location.replace('login.html');
+//         $($(".nav-item")[0]).html("<a class=\"nav-link\" href=\"login.html\">Login<span class=\"sr-only\">(current)</span></a>");
+//         $($(".nav-item")[1]).html("<a class=\"nav-link\" href=\"register.html\">Register<span class=\"sr-only\">(current)</span></a>");
+//         $($(".nav-item")[1]).addClass("active");
+//         $($(".nav-item")[1]).removeClass("dropdown");
+//         console.log("Not logged in");
+    });
+}
+
 function checkLogin() {
     if(request)
         request.abort();
@@ -27,23 +92,27 @@ function checkLogin() {
 function initialize() {
     $('.dummy').matchHeight();
     checkLogin();
+
     //todo: calendar starts
     let calDiv = document.getElementById("divCal");
-    let calendar = new Calendar(calDiv, new Date().getTime(),20,"Pick a date");
+    calendar = new Calendar(calDiv, new Date().getTime(),20,"Pick a date");
     //todo: to get date range
     console.log(calendar.getLimits());
     //todo: supply a badge array
     //length should be Math.floor(range*1.5)
-    console.log([0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,1,0,0,1,1].length);
-    calendar.setBadges([0,0,0,0,0,0,0,0,1,1.0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,1,0,0,1,1]);
+// 		console.log([0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0,1,0,0,1,1].length);
+    tinySchedule();
     calendar.render();
+
     calDiv.addEventListener('calendarSelect',
         function(){
             //todo: this is how you get the date
             //I am using my own calendartime object, read the structure in console
             console.log(calendar.selectionDate);
+
         }
     );
+
 
 }
 
